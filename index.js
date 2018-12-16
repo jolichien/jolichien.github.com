a5c299ab8618f5bfeacef662997d6fe21b5c65f9
Joli Chien <jolichien@Jolis-MacBook-Pro.local>
2018-12-16 15:36:33 -0800

    pushing code to github repository for first time

---
+++ index.js
@@ -0,0 +1,14 @@
+$("#workDropDown").hover(function() {
+        $('#menu').css("visibility", "visible");
+    },
+    function() {
+        $('#menu').css("visibility", "hidden");
+    }
+);
+$("#menu").hover(function() {
+    $('#menu').css("visibility", "visible");
+    },
+    function() {
+        $('#menu').css("visibility", "hidden");
+    }
+);
