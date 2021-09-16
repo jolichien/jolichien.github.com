3de606c82e4f6ef28915de529746eff2fcaa61c7
jolichien <45911967+jolichien@users.noreply.github.com>
2021-09-15 23:28:01 -0700

    Update Content
    Added images, styling, animations, etc.

---
+++ index.js
@@ -0,0 +1,30 @@
+$(".dropbtn").hover(function() {
+    $('.dropdown-content').css("visibility", "visible");
+},
+function() {
+    $('.dropdown-content').css("visibility", "hidden");
+}
+);
+$(".dropdown-content").hover(function() {
+$('.dropdown-content').css("visibility", "visible");
+},
+function() {
+    $('.dropdown-content').css("visibility", "hidden");
+}
+);
+
+
+$(".dropbtn").click(function() {
+    $('.dropdown-content').css("visibility", "visible");
+},
+function() {
+    $('.dropdown-content').css("visibility", "hidden");
+}
+);
+$(".dropdown-content").click(function() {
+$('.dropdown-content').css("visibility", "visible");
+},
+function() {
+    $('.dropdown-content').css("visibility", "hidden");
+}
+);
